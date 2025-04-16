b6048428286ea8cecd06544766c3bf66bd31dbdf
AIPLBLRNT0021\Karthik R <Karthik.r@anorasolutions.com>
2025-04-16 17:05:27 +0530

    no message

--- e.py
+++ e.py
@@ -1,7 +1,25 @@
-import csv
+'''import csv
 
 with open("sample.txt",'r') as file:
     content=file.read()
 
 print(content)
+'''
 
+s="(()()())"
+
+l=[]
+c=0
+
+for i in s:
+    if i=='(':
+        c+=1
+    elif i==')':
+        c-=1
+    if c<0:
+        print("False")
+        break
+if c==0:
+    print("True")
+elif c>0:
+    print("False")
